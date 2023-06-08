9dd6f4becb499e6bd59779a0c2473e1b11d36546
westetter <westley.tetter@amtote.com>
2023-06-08 15:28:54 -0400

    adding pin generator script

---
+++ pin_generator.sh
@@ -0,0 +1,12 @@
+pins=$1  
+
+if [[ -z $pins ]]; then
+    echo "Please provide the desired count as an argument."
+    exit 1
+fi
+
+for ((i=1; i<=pins; i++))
+do
+    random_number=$((RANDOM % 9000 + 1000))  
+    echo $random_number
+done
